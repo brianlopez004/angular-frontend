d0f5d4435e5e97473f44f0d721c918c30488b80c
Braian López <braianortiz1603@gmail.com>
2023-09-23 12:22:02 -0500

    feat: Creation of the initial components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,5 @@
+//Archivo de pruebas unitarias
+
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
