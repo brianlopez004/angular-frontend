5518133aa44e4a38dad36d0abeb8d0650a693a1d
Braian López <braianortiz1603@gmail.com>
2023-09-23 16:21:16 -0500

    feat: Creating paths for the dashboard and logging in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
 
-const routes: Routes = [];
+//Array con las rutas
+const routes: Routes = [
+  {path: '', redirectTo:'/inicio', pathMatch:'full'},
+  { path: 'inicio', component: DashboardComponent },
+  { path: 'inicio-sesion', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
